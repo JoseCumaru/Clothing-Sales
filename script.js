6ade2cb032cfd8a02625efadf5b4840f89761f08
Jose Cumaru Neto <josecumaru18@gmail.com>
2024-03-07 12:53:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function chamarWhatsapp(){
+    var number = "+5597984398480"
+
+    var url = "https://api.whatsapp.com/send?phone=1234567890" + numeroTelefone;
+
+    window.location.href = url;
+    
+}
+
+function chamarEmail(){
+
+}
+
