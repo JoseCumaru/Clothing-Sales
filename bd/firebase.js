00910694ccdec2d0be5389801a0aa1c5cca2a750
Jose Cumaru Neto <josecumaru18@gmail.com>
2024-03-12 21:30:36 -0300

    catalog updated

--- bd/firebase.js
+++ bd/firebase.js
@@ -11,43 +11,97 @@ const firebaseConfig = {
   appId: "1:856958563843:web:7758d4e8948316a5e44036"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
 
-
-const db = getFirestore(app);
-
-// Get a list of cities from your database
-async function getRoupas(db) {
-  const roupasCol = collection(db, 'Roupas', 'Feminino', 'Adulto');
-  const roupasSnapshot = await getDocs(roupasCol);
-  const roupasList = roupasSnapshot.docs.map(doc => doc.data());
-  return roupasList;
-}
-
+// Obtenha uma referência para o Firestore
+const db = firebase.firestore();
 
 // Função para recuperar e exibir roupas femininas para adultos
 async function getRoupasFemininasAdulto() {
-  const resultadosDiv = document.getElementById('resultados');
-  resultadosDiv.innerHTML = ''; // Limpa o conteúdo anterior
-  
-  const roupasRef = collection(db, 'Roupas', 'Feminino', 'Adulto');
-  const snapshot = await getDocs(roupasRef);
-  
-  snapshot.forEach(doc => {
-    // Crie elementos HTML para exibir os dados
-    const roupaDiv = document.createElement('div');
-    roupaDiv.innerHTML = `
-      <img>${doc.data().img}<img>
-      <h3>${doc.data().nome}</h3>
-      <p>valor: ${doc.data().valor}</p>
-      <!-- Adicione mais campos conforme necessário -->
-    `;
-    
-    // Adicione o elemento da roupa à div de resultados
-    resultadosDiv.appendChild(roupaDiv);
-  });
+    const resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = ''; // Limpa o conteúdo anterior
+
+    const roupasRef = db.collection('Roupas').doc('Feminino').collection('Adulto');
+    const snapshot = await roupasRef.get();
+
+    snapshot.forEach(doc => {
+        // Crie elementos HTML para exibir os dados
+        const roupaDiv = document.createElement('div');
+        roupaDiv.innerHTML = `
+            <img src="${doc.data().img} alt="Imagem da Roupa">
+            <h3>${doc.data().nome}</h3>
+            <p>Valor: ${doc.data().valor}</p>
+            <!-- Adicione mais campos conforme necessário -->
+            `;
+
+            // Adicione o botão "Adicionar ao Carrinho"
+            const btnAdicionar = document.createElement('button');
+            btnAdicionar.textContent = 'Adicionar ao Carrinho';
+            btnAdicionar.classList.add('btn-adicionar');
+            roupaDiv.appendChild(btnAdicionar);
+
+            // Adicione o elemento da roupa à div de resultados
+            resultadosDiv.appendChild(roupaDiv);
+            });
+        }
+
+        // Chame a função para recuperar e exibir as roupas quando necessário
+        getRoupasFemininasAdulto();
+
+
+async function getRoupasFemininasAdulto() {
+    const resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = ''; // Limpa o conteúdo anterior
+
+    const roupasRef = db.collection('Roupas').doc('Feminino').collection('Adulto');
+    const snapshot = await roupasRef.get();
+
+    snapshot.forEach(doc => {
+        // Crie elementos HTML para exibir os dados
+        const roupaDiv = document.createElement('div');
+        roupaDiv.innerHTML = `
+            <img src="${doc.data().img} alt="Imagem da Roupa">
+            <h3>${doc.data().nome}</h3>
+            <p>Valor: ${doc.data().valor}</p>
+            <!-- Adicione mais campos conforme necessário -->
+            `;
+
+            // Adicione o botão "Adicionar ao Carrinho"
+            const btnAdicionar = document.createElement('button');
+            btnAdicionar.textContent = 'Adicionar ao Carrinho';
+            btnAdicionar.classList.add('btn-adicionar');
+            roupaDiv.appendChild(btnAdicionar);
+
+            // Adicione o elemento da roupa à div de resultados
+            resultadosDiv.appendChild(roupaDiv);
+      });
 }
 
-// Chame a função para recuperar e exibir as roupas quando necessário
-getRoupasFemininasAdulto();
+async function getRoupasMasculinosAdulto() {
+  const resultadosDiv = document.getElementById('resultados');
+  resultadosDiv.innerHTML = ''; // Limpa o conteúdo anterior
+
+  const roupasRef = db.collection('Roupas').doc('Masculinos').collection('Adulto');
+  const snapshot = await roupasRef.get();
+
+  snapshot.forEach(doc => {
+      // Crie elementos HTML para exibir os dados
+      const roupaDiv = document.createElement('div');
+      roupaDiv.innerHTML = `
+          <img src="${doc.data().img} alt="Imagem da Roupa">
+          <h3>${doc.data().nome}</h3>
+          <p>Valor: ${doc.data().valor}</p>
+          <!-- Adicione mais campos conforme necessário -->
+          `;
+
+          // Adicione o botão "Adicionar ao Carrinho"
+          const btnAdicionar = document.createElement('button');
+          btnAdicionar.textContent = 'Adicionar ao Carrinho';
+          btnAdicionar.classList.add('btn-adicionar');
+          roupaDiv.appendChild(btnAdicionar);
+
+          // Adicione o elemento da roupa à div de resultados
+          resultadosDiv.appendChild(roupaDiv);
+    });
+}
